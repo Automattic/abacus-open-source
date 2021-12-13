aedcc83ede08b5a95bcd6d0bd0b8fef3291e68b0
Isaac Sung <isaacsung@gmail.com>
2021-12-13 14:03:21 -0600

    Add typescript definitions for smaller dist of plotly

--- src/components/general/LazyPlot.tsx
+++ src/components/general/LazyPlot.tsx
@@ -1,8 +1,8 @@
 import { LinearProgress } from '@material-ui/core'
-import React, { Suspense } from 'react'
+import { Alert } from '@material-ui/lab'
+import React, { useEffect, useRef, useState } from 'react'
 import { PlotParams } from 'react-plotly.js'
-
-const Plot = React.lazy(() => import('react-plotly.js'))
+import createPlotlyComponent from 'react-plotly.js/factory'
 
 /**
  * A code-split version of a React Plotly plot in order to reduce memory usage. Displays a loading bar while it lazily loads react-plotly.
@@ -10,9 +10,33 @@ const Plot = React.lazy(() => import('react-plotly.js'))
  * @returns A Plotly Plot component.
  */
 export default function LazyPlot(props: PlotParams): JSX.Element {
+  const Plot = useRef<React.ComponentType<PlotParams> | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    import('plotly.js-cartesian-dist')
+      .then(({ default: plotly }) => {
+        Plot.current = createPlotlyComponent(plotly)
+        setLoading(false)
+        return false
+      })
+      .catch((_) => {
+        setLoading(false)
+        setError(true)
+      })
+  }, [loading])
+
   return (
-    <Suspense fallback={<LinearProgress />}>
-      <Plot {...props} />
-    </Suspense>
+    <>
+      {loading ? (
+        <LinearProgress />
+      ) : error ? (
+        <Alert severity='error'>Chart could not be loaded.</Alert>
+      ) : (
+        // eslint-disable-next-line
+        <Plot.current {...props} />
+      )}
+    </>
   )
 }
