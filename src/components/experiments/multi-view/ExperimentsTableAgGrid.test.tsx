00ea1fc2e688c885b35f8b1ef4bfe58401e40197
Isaac Sung <isaacsung@gmail.com>
2022-01-05 11:41:15 -0600

    Update tests

--- src/components/experiments/multi-view/ExperimentsTableAgGrid.test.tsx
+++ src/components/experiments/multi-view/ExperimentsTableAgGrid.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import addToDate from 'date-fns/add'
 import { createMemoryHistory } from 'history'
@@ -9,7 +9,7 @@ import { ExperimentBare, Platform, Status } from 'src/lib/schemas'
 import { changeFieldByRole, render } from 'src/test-helpers/test-utils'
 
 import ExperimentsTableAgGrid from './ExperimentsTableAgGrid'
-import { getParamsStringFromObj } from './ExperimentsTableAgGrid.utils'
+import { defaultSortParams, getParamsStringFromObj, UrlParams } from './ExperimentsTableAgGrid.utils'
 
 it('should render an empty table', () => {
   const { container } = render(<ExperimentsTableAgGrid experiments={[]} />)
@@ -34,7 +34,73 @@ it('should render a table with experiments', async () => {
   expect(container).toMatchSnapshot()
 })
 
-it('should allow searching and resetting by changing url params', async () => {
+it('should allow searching, sorting, and resetting by changing url params', async () => {
+  const history = createMemoryHistory()
+  const experiments: ExperimentBare[] = [
+    {
+      experimentId: 1,
+      name: 'First',
+      endDatetime: addToDate(new Date(), { days: 14 }),
+      ownerLogin: 'Owner',
+      platform: Platform.Wpcom,
+      startDatetime: new Date(),
+      status: Status.Staging,
+    },
+  ]
+  const { container } = render(
+    <Router history={history}>
+      <ExperimentsTableAgGrid experiments={experiments} />
+    </Router>,
+  )
+
+  await screen.findByText(/First/)
+
+  // Wait for default sorting options
+  let expectedParamsObj = defaultSortParams as UrlParams
+  await waitFor(() => {
+    expect(history.length).toBe(2)
+  })
+  expect(history.location.search).toBe(`?${getParamsStringFromObj(expectedParamsObj)}`)
+
+  // Test search bar
+  const searchString = 'explat_test'
+  expectedParamsObj = {
+    ...expectedParamsObj,
+    search: searchString,
+  }
+  await changeFieldByRole('textbox', /Search/, searchString)
+  expect(history.length).toBe(3)
+  expect(history.location.search).toBe(`?${getParamsStringFromObj(expectedParamsObj)}`)
+
+  // Test sorting
+  expectedParamsObj = {
+    nameS: 'asc',
+    nameSi: '0',
+    search: searchString,
+  }
+  const nameColumn = screen.getByText(/Name/)
+  userEvent.click(nameColumn)
+  await waitFor(() => {
+    container.querySelectorAll('.ag-header-cell-label').forEach((value, _key, _parent) => {
+      const headerText = value.querySelector('.ag-header-cell-text')?.innerHTML
+      if (headerText === 'Name') {
+        expect(value.querySelector('.ag-icon-asc')).toBeInTheDocument()
+      }
+    })
+  })
+  expect(history.length).toBe(4)
+  expect(history.location.search).toBe(`?${getParamsStringFromObj(expectedParamsObj)}`)
+
+  // Test reset
+  expectedParamsObj = defaultSortParams
+  const resetButton = screen.getByRole('button', { name: /Reset/ })
+  userEvent.click(resetButton)
+  await screen.findByText(/wpcom/)
+  expect(history.length).toBe(5)
+  expect(history.location.search).toBe(`?${getParamsStringFromObj(expectedParamsObj)}`)
+})
+
+it('getting rid of all sorting should result in special url params', async () => {
   const history = createMemoryHistory()
   const experiments: ExperimentBare[] = [
     {
@@ -55,19 +121,29 @@ it('should allow searching and resetting by changing url params', async () => {
 
   await screen.findByText(/First/)
 
-  // Test search bar
-  const searchString = 'explat_test'
-  const expectedParamsObj = {
-    search: searchString,
-  }
-  await changeFieldByRole('textbox', /Search/, searchString)
-  expect(history.length).toBe(2)
+  // Wait for default sorting options
+  let expectedParamsObj = defaultSortParams as UrlParams
+  await waitFor(() => {
+    expect(history.length).toBe(2)
+  })
   expect(history.location.search).toBe(`?${getParamsStringFromObj(expectedParamsObj)}`)
 
-  // Test reset
-  const resetButton = screen.getByRole('button', { name: /Reset/ })
-  userEvent.click(resetButton)
-  await screen.findByText(/wpcom/)
-  expect(history.length).toBe(3)
-  expect(history.location.search).toBe('')
+  // Test sorting
+  expectedParamsObj = {
+    null: 'true',
+  }
+  const nameColumn = screen.getByText(/Name/)
+  userEvent.click(nameColumn)
+  await waitFor(() => {
+    expect(history.length).toBe(3)
+  })
+  userEvent.click(nameColumn)
+  await waitFor(() => {
+    expect(history.length).toBe(4)
+  })
+  userEvent.click(nameColumn)
+  await waitFor(() => {
+    expect(history.length).toBe(5)
+  })
+  expect(history.location.search).toBe(`?${getParamsStringFromObj(expectedParamsObj)}`)
 })
