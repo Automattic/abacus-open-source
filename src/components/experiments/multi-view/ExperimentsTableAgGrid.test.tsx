442db32af87c33a3ba966319ac03da8450b24394
Isaac Sung <isaacsung@gmail.com>
2022-01-05 10:57:43 -0600

    Update tests and snapshot

--- src/components/experiments/multi-view/ExperimentsTableAgGrid.test.tsx
+++ src/components/experiments/multi-view/ExperimentsTableAgGrid.test.tsx
@@ -1,11 +1,15 @@
-import { fireEvent, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import addToDate from 'date-fns/add'
+import { createMemoryHistory } from 'history'
 import React from 'react'
+import { Router } from 'react-router-dom'
 
 import { ExperimentBare, Platform, Status } from 'src/lib/schemas'
 import { changeFieldByRole, render } from 'src/test-helpers/test-utils'
 
 import ExperimentsTableAgGrid from './ExperimentsTableAgGrid'
+import { getParamsStringFromObj } from './ExperimentsTableAgGrid.utils'
 
 it('should render an empty table', () => {
   const { container } = render(<ExperimentsTableAgGrid experiments={[]} />)
@@ -13,7 +17,7 @@ it('should render an empty table', () => {
   expect(container).toMatchSnapshot()
 })
 
-it('should render a table with experiments, allow searching and resetting', async () => {
+it('should render a table with experiments', async () => {
   const experiments: ExperimentBare[] = [
     {
       experimentId: 1,
@@ -28,11 +32,42 @@ it('should render a table with experiments, allow searching and resetting', asyn
   const { container } = render(<ExperimentsTableAgGrid experiments={experiments} />)
 
   expect(container).toMatchSnapshot()
+})
 
-  await changeFieldByRole('textbox', /Search/, 'explat_test')
-  expect(container).toMatchSnapshot()
+it('should allow searching and resetting by changing url params', async () => {
+  const history = createMemoryHistory()
+  const experiments: ExperimentBare[] = [
+    {
+      experimentId: 1,
+      name: 'First',
+      endDatetime: addToDate(new Date(), { days: 14 }),
+      ownerLogin: 'Owner',
+      platform: Platform.Wpcom,
+      startDatetime: new Date(),
+      status: Status.Staging,
+    },
+  ]
+  render(
+    <Router history={history}>
+      <ExperimentsTableAgGrid experiments={experiments} />
+    </Router>,
+  )
 
+  await screen.findByText(/First/)
+
+  // Test search bar
+  const searchString = 'explat_test'
+  const expectedParamsObj = {
+    search: searchString,
+  }
+  await changeFieldByRole('textbox', /Search/, searchString)
+  expect(history.length).toBe(2)
+  expect(history.location.search).toBe(`?${getParamsStringFromObj(expectedParamsObj)}`)
+
+  // Test reset
   const resetButton = screen.getByRole('button', { name: /Reset/ })
-  fireEvent.click(resetButton)
-  expect(container).toMatchSnapshot()
+  userEvent.click(resetButton)
+  await screen.findByText(/wpcom/)
+  expect(history.length).toBe(3)
+  expect(history.location.search).toBe('')
 })
