0a7ce631bb76b8eaea363adfe13c9640b3a6069f
Isaac Sung <isaacsung@gmail.com>
2022-01-12 15:43:25 -0600

    Address Jessie's comments

--- src/components/general/AgGridWithDetails.utils.tsx
+++ src/components/general/AgGridWithDetails.utils.tsx
@@ -1,7 +1,7 @@
-import { CellClickedEvent, GetRowNodeIdFunc, GridApi } from 'ag-grid-community'
+import { CellClickedEvent, GetRowNodeIdFunc, GridApi, ICellRendererParams } from 'ag-grid-community'
 import React from 'react'
 
-import RowToggleButton from './RowToggleButton'
+import RotatingToggleButton from './RotatingToggleButton'
 
 export const DETAIL_TOGGLE_BUTTON_COLUMN_NAME = '__detail-button-col__'
 const DETAIL_ID_SUFFIX = '-detail'
@@ -16,7 +16,7 @@ export const detailIdFromDataId = (dataId: string): string => {
 export const setHeightOfFullWidthRow = (
   rowIndex: number,
   data: Record<string, unknown>,
-  maxRowHeightMap: Map<string, number>,
+  maxRowHeightMap: Record<string, number>,
   gridApiRef: GridApi | null,
   getRowNodeId: GetRowNodeIdFunc,
 ): void => {
@@ -49,7 +49,7 @@ export const setHeightOfFullWidthRow = (
 
     const rowChild = found.firstElementChild
     const rowHeight = rowChild.clientHeight
-    maxRowHeightMap.set(detailIdFromDataId(getRowNodeId(data)), rowHeight)
+    maxRowHeightMap[detailIdFromDataId(getRowNodeId(data))] = rowHeight
 
     gridApiRef?.resetRowHeights()
     //gridApiRef?.redrawRows()
@@ -58,58 +58,12 @@ export const setHeightOfFullWidthRow = (
 
 export const isFullWidth = (data: Record<string, unknown> & { isDetail?: boolean }): boolean => data.isDetail === true
 
-export const addDetailRow = (
-  data: Record<string, unknown>,
-  rowData: Record<string, unknown>[],
-  getRowNodeId: GetRowNodeIdFunc,
-): Record<string, unknown>[] => {
-  const newMetrics = [...rowData]
-  const index = newMetrics.findIndex((element) => getRowNodeId(element) === getRowNodeId(data))
-  newMetrics.splice(index + 1, 0, { ...data, isDetail: true })
-  return newMetrics
-}
-
-export const removeDetailRow = (
-  data: Record<string, unknown>,
-  rowData: Record<string, unknown>[],
-  getRowNodeId: GetRowNodeIdFunc,
-): Record<string, unknown>[] => {
-  const newMetrics = [...rowData]
-  const index = newMetrics.findIndex(
-    (element) => getRowNodeId(element) === getRowNodeId(data) && element.isDetail === true,
-  )
-
-  // istanbul ignore next; trivial and shouldn't occur
-  if (index >= 0) {
-    newMetrics.splice(index, 1)
-  }
-  return newMetrics
-}
-
-export const toggleDetailRow = (
-  data: Record<string, unknown>,
-  rowData: Record<string, unknown>[],
-  detailRowToggleMap: Map<string, boolean>,
-  getRowNodeId: GetRowNodeIdFunc,
-): Record<string, unknown>[] => {
-  const detailRowExists = !!detailRowToggleMap.get(getRowNodeId(data))
-  let newRows
-  if (detailRowExists) {
-    newRows = removeDetailRow(data, rowData, getRowNodeId)
-  } else {
-    newRows = addDetailRow(data, rowData, getRowNodeId)
-  }
-
-  detailRowToggleMap.set(getRowNodeId(data), !detailRowExists)
-  return newRows
-}
-
 export const getRowHeight = (
   data: Record<string, unknown>,
-  { maxRowHeightMap, getRowNodeId }: { maxRowHeightMap: Map<string, number>; getRowNodeId: GetRowNodeIdFunc },
+  { maxRowHeightMap, getRowNodeId }: { maxRowHeightMap: Record<string, number>; getRowNodeId: GetRowNodeIdFunc },
 ): number | undefined | null => {
   if (isFullWidth(data)) {
-    const result = maxRowHeightMap.get(detailIdFromDataId(getRowNodeId(data))) as number
+    const result = maxRowHeightMap[detailIdFromDataId(getRowNodeId(data))]
     if (typeof result === 'undefined') {
       // a barely visible height as it renders so we can load the true height
       return 1
@@ -123,68 +77,93 @@ export const onCellClicked = (
   event: CellClickedEvent,
   {
     gridApiRef,
-    rowData,
-    setRowData,
     detailRowToggleMap,
+    setDetailRowToggleMap,
     maxRowHeightMap,
     getRowNodeId,
     actionColumnIdSuffix,
   }: {
     gridApiRef: GridApi | null
-    rowData: Record<string, unknown>[]
-    setRowData: (rowData: Record<string, unknown>[]) => void
-    detailRowToggleMap: Map<string, boolean>
-    maxRowHeightMap: Map<string, number>
+    detailRowToggleMap: Record<string, boolean>
+    setDetailRowToggleMap: (map: Record<string, boolean>) => void
+    maxRowHeightMap: Record<string, number>
     getRowNodeId: GetRowNodeIdFunc
     actionColumnIdSuffix?: string | undefined
   },
 ): void => {
+  // istanbul ignore next; trivial
+  if (!gridApiRef || isFullWidth(event.data)) {
+    return
+  }
+
   // Ignore clicks on cells with 'actions'
   if (actionColumnIdSuffix && event.column.getColId().endsWith(actionColumnIdSuffix)) {
     return
   }
 
-  // istanbul ignore else
-  if (!isFullWidth(event.data) && getRowNodeId && gridApiRef) {
-    const target = event.event?.target as HTMLElement
+  // Retrieve the HTML element of the detail toggle button
+  const target = event.event?.target as HTMLElement
+  const params = { columns: [DETAIL_TOGGLE_BUTTON_COLUMN_NAME], rowNodes: [event.node] }
+  const instances = gridApiRef?.getCellRendererInstances(params)
 
-    // Retrieve the HTML element of the detail toggle button
-    const params = { columns: [DETAIL_TOGGLE_BUTTON_COLUMN_NAME], rowNodes: [event.node] }
-    const instances = gridApiRef?.getCellRendererInstances(params)
+  // istanbul ignore next; precautionary check, shouldn't occur
+  if (instances.length === 0) {
+    return
+  }
 
-    // istanbul ignore else
-    if (instances.length > 0) {
-      const instance = instances[0].getGui()
-      const button = instance.firstElementChild
+  const instance = instances[0]
+  const instanceGui = instance.getGui()
+  const element = instanceGui.firstElementChild as HTMLElement
 
-      // Toggle the detail row if the detail button was clicked
-      if (target && (target === button || button?.contains(target))) {
-        setRowData(toggleDetailRow(event.data, rowData, detailRowToggleMap, getRowNodeId))
+  // Toggle the detail row if the detail button was clicked
+  if (target && (target === element || element?.contains(target))) {
+    const rowId = getRowNodeId(event.data)
+    const toggledValue = !detailRowToggleMap[rowId]
+    setDetailRowToggleMap({
+      ...detailRowToggleMap,
+      [rowId]: toggledValue,
+    })
 
-        // istanbul ignore else
-        if (detailRowToggleMap.get(getRowNodeId(event.data))) {
-          setHeightOfFullWidthRow(1 + (event.rowIndex as number), event.data, maxRowHeightMap, gridApiRef, getRowNodeId)
-        }
-      }
-      // Trigger the detail button if anything else in the row was clicked
-      else {
-        const element = instance.firstElementChild as HTMLElement
-        element.click()
-      }
+    if (toggledValue) {
+      setHeightOfFullWidthRow(1 + (event.rowIndex as number), event.data, maxRowHeightMap, gridApiRef, getRowNodeId)
     }
+
+    // Note: there appears to be a bug with using gridApiRef.refreshCells() in that it doesn't update the row data params for the cell renderer
+    // Thus, must manually refresh the button renderer with explicit params
+    instance.refresh({
+      data: { ...event.data, isOpen: toggledValue } as keyof ICellRendererParams,
+    } as ICellRendererParams)
+  }
+  // Trigger the detail button if anything else in the row was clicked
+  else {
+    element.click()
   }
 }
 
-export const DetailToggleButtonRenderer = ({
-  data,
-  getRowNodeId,
-  detailRowToggleMap,
-}: {
-  data: Record<string, unknown>
-  getRowNodeId: GetRowNodeIdFunc
-  detailRowToggleMap: Map<string, boolean>
-}): JSX.Element => {
-  const toggled = !!detailRowToggleMap.get(getRowNodeId(data))
+export const getRowDataWithDetails = (
+  rowData: Record<string, unknown>[],
+  detailRowToggleMap: Record<string, boolean>,
+  getRowNodeId: GetRowNodeIdFunc,
+): Record<string, unknown>[] => {
+  const detailRows = rowData
+    .filter((row) => {
+      return detailRowToggleMap[getRowNodeId(row)]
+    })
+    .map((row) => {
+      return { ...row, isDetail: true }
+    })
+  const toggledRows = rowData.map((row) => {
+    if (detailRowToggleMap[getRowNodeId(row)]) {
+      return { ...row, isOpen: true }
+    } else {
+      return row
+    }
+  })
 
-  return <RowToggleButton toggled={toggled} />
+  return [...toggledRows, ...detailRows]
+}
+
+export const DetailButtonRenderer = ({ data }: { data: Record<string, unknown> }): JSX.Element => {
+  const toggled = !!data.isOpen
+  return <RotatingToggleButton isOpen={toggled} />
 }
